917ca4aa32a1dc40ff11d05a0105bfa75439353e
Elena <ed41658@gmail.com>
2025-02-02 12:37:58 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return <div>hello!</div>
+}
+
+export default App
+
+/* 
+
+create git rep -> commits
+
+folder components
+
+scss
+classnames (module)
+
+*/
